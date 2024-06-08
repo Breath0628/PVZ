d47f6ec4b48a6181807719561a437d273d4c8789
Breath <2974413514@qq.com>
2024-06-08 22:05:18 +0800

    增加了摄像机类，测试摄像机移动逻辑，完善向量类方法

--- menu_scene.h
+++ menu_scene.h
@@ -4,11 +4,11 @@
 #include <iostream>
 #include "scene_manager.h"
 #include "animation.h"
+#include "camera.h"
 
 using namespace std;
 
 extern SceneManager* scene_manager;
-
 extern Atlas atlas_peashooter_run_right;
 
 class MenuScene:public Scene
@@ -22,18 +22,17 @@ public:
 		animation_peashooter_run_right.set_atlas(&atlas_peashooter_run_right);
 		animation_peashooter_run_right.set_interval(75);
 		animation_peashooter_run_right.set_loop(1);
-		animation_peashooter_run_right.set_callback([]() {
-			scene_manager->switch_to(SceneManager::SceneType::Game);
-			});
+		
 	}
 	void on_update(int delta){
 		//cout << "Main Menu is running!" << endl;
-		
+		camera.on_update(delta);
 		animation_peashooter_run_right.on_update(delta);
 	}
 	void on_draw(){
 		//outtextxy(10, 10, _T("Main Menu"));
-		animation_peashooter_run_right.on_draw(100, 100);
+		const Vector2<float>& pos_camera = camera.getPos();
+		animation_peashooter_run_right.on_draw((int)(100-pos_camera.x), (int)(100- pos_camera.y));//由摄像机位置与世界位置得到窗口位置
 	}
 	void on_input(const ExMessage &msg) {
 		if (msg.message == WM_KEYDOWN) {
@@ -46,4 +45,5 @@ public:
 
 private:
 	Animation animation_peashooter_run_right;
+	Camera camera;
 };
