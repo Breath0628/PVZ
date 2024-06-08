fab15b800f909d916500c002ba19c088b273b720
Breath <2974413514@qq.com>
2024-06-08 21:10:57 +0800

    实现Vector2向量模板类,实现向量基本的运算逻辑方法

---
+++ vector2.h
@@ -0,0 +1,62 @@
+#pragma once
+#include <cmath>
+template<class T>
+
+class Vector2
+{
+public:
+	T x = 0;
+	T y = 0;
+
+public:
+	Vector2() = default;
+	~Vector2() = default;
+
+	Vector2(T x, T y) :x(x), y(y) {}
+
+	Vector2<T> operator+(const Vector2<T>& vec)const {
+		return Vector2<T>(x + vec.x, y + vec.y);
+	}
+
+	Vector2<T> operator-(const Vector2<T>& vec)const {
+		return Vector2<T>(x - vec.x, y - vec.y);
+	}
+
+
+	T operator*(const Vector2<T>& vec)const//向量相乘 返回值为数值
+	{
+		return x * vec.x + y * vec.y;
+	}
+
+	void operator+=(const Vector2<T>& vec) {
+		x += vec.x;
+		y += vec.y;
+	}
+
+
+	void operator-=(const Vector2<T>& vec) {
+		x -= vec.x;
+		y -= vec.y;
+	}
+
+	void operator*=(T val)//向量乘数值 返回值为向量
+	{
+		x = val * x;
+		y = val * y;
+	}
+
+	float length() //返回向量长度
+	{
+		return sqrt(x * x + y * y);
+	}
+
+	Vector2<T> normalize()//向量标准化
+	{
+		float len = length();
+		if (len == 0) return Vector2<T>(0, 0);
+		return Vector2<T>(x / len, y / len);
+	}
+
+private:
+
+};
